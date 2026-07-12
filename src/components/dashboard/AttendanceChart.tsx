2b4c3bc9cfd77cadd149a99b4506c3e6f6d4eb99
Shah Faisal <faisalagentai@gmail.com>
2026-07-13 01:11:07 +0500

    Remove explicit type on Tooltip formatter value param

--- src/components/dashboard/AttendanceChart.tsx
+++ src/components/dashboard/AttendanceChart.tsx
@@ -67,7 +67,7 @@ export function AttendanceChart({ data, loading }: AttendanceChartProps) {
                 axisLine={false}
               />
               <Tooltip
-                formatter={(value: number | undefined) => [`${value ?? 0}%`, 'Present']}
+                formatter={(value) => [`${value ?? 0}%`, 'Present']}
                 contentStyle={{
                   fontSize: 12,
                   borderRadius: 6,
